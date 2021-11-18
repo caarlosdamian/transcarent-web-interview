6710fb85eb4d05ea8b518786625ea088f70c47c7
caarlosdamian <dapecarlos@gmail.com>
2021-11-18 14:18:51 -0600

    Let's add interactions

--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,14 +1,47 @@
+import { useEffect, useState } from "react";
 import { data } from "./data";
 import "./index.css";
 
 export default function Tree() {
+  const [newAnimal, setnewAnimal] = useState();
+  const [animales, setAnimales] = useState(data);
+  useEffect(() => {}, [animales]);
+
+  const addNewAnimal = (e) => {
+    if (e.key === "Enter") {
+      setAnimales([...animales, { name: newAnimal, id: animales.length + 1 }]);
+    }
+    return;
+  };
+  const deleteAnimal = (animalToDelete) => {
+    setAnimales(
+      animales.filter((val) => {
+        return val.name !== animalToDelete;
+      })
+    );
+  };
+  console.log(animales);
   return (
     <div className="tree">
-      {data.map((animal) => (
-        <p className={animal?.className} key={animal.id}>
-          {animal.name}
-        </p>
+      {animales?.map((animal, key) => (
+        <div
+          key={key}
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <p className={animal.className}>{animal.name}</p>
+          <span
+            style={{ cursor: "pointer" }}
+            onClick={() => deleteAnimal(animal.name)}
+          >
+            ❌
+          </span>
+        </div>
       ))}
+      <input
+        type="text"
+        onKeyDown={addNewAnimal}
+        onChange={(e) => setnewAnimal(e.target.value)}
+      />
     </div>
   );
 }
