747c0dc09d470324315114e29987016a66993ae4
caarlosdamian <dapecarlos@gmail.com>
2021-11-18 13:31:58 -0600

    Next, with data

---
+++ trees/src/tree/data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    name: "root",
+    className: "",
+  },
+  {
+    id: 2,
+    name: "ant",
+    className: "p2",
+  },
+  {
+    id: 3,
+    name: "bear",
+    className: "p2",
+  },
+
+  {
+    id: 4,
+    name: "cat",
+    className: "p3",
+  },
+  {
+    id: 5,
+    name: "dog",
+    className: "p3",
+  },
+  {
+    id: 6,
+    name: "elephant",
+    className: "p4",
+  },
+  {
+    id: 7,
+    name: "frog",
+    className: "p2",
+  },
+];
--- trees/src/tree/index.js
+++ trees/src/tree/index.js
@@ -1,15 +1,14 @@
+import { data } from "./data";
 import "./index.css";
 
 export default function Tree() {
   return (
     <div className="tree">
-      <p>root</p>
-      <p className="p2">ant</p>
-      <p className="p2">bear</p>
-      <p className="p3">cat</p>
-      <p className="p3">dog</p>
-      <p className="p4">elephant</p>
-      <p className="p2">frog</p>
+      {data.map((animal) => (
+        <p className={animal?.className} key={animal.id}>
+          {animal.name}
+        </p>
+      ))}
     </div>
   );
 }
